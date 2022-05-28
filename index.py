e4aa36673e8e352330115c0dcf4d8a37a6bb62d2
keji-kay <kejibeth@gmail.com>
2022-05-28 08:04:18 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+
+class Student:
+  def __init__(self, name, age,score,tracks):
+    self.name = name
+    self.age = age
+    self.score = score
+    self.tracks =tracks
+
+Bolu = Student("Bob", 26 ,20.0, ["FE","BE"])
+
+print(Bob.name)
+print(Bob.age)
+print(Bob.score)
+print(Bob.tracks)
